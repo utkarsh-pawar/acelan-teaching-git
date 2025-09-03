ec326398c44f116e6fc12bb89960cde0b511b183
AT-03 <at-03@pop-os.localdomain>
2025-09-03 14:20:28 +0530

    initialized some math functions

---
+++ index.js
@@ -0,0 +1,21 @@
+// adding two numbers
+const add = (a, b) => {
+  return a + b;
+};
+
+// subtracting two numbers
+const subtract = (a, b) => {
+  return a - b;
+};
+
+// multiplying two numbers
+const multiply = (a, b) => {
+  return a * b;
+};
+
+// dividing two numbers 
+const divide = (a, b) => {
+  return a / b;
+};
+
+console.log(add(3, 10))
